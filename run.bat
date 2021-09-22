5d20064326fd27b91cd9cd6ccd3bb0984b9b1377
OfrK <montenegro37@outlook.com>
2021-09-22 14:04:57 +0300

    init

---
+++ run.bat
@@ -0,0 +1,4 @@
+:loop
+    "AirtestIDE\AirtestIDE" runner "main.air"  --device android://127.0.0.1:5037/127.0.0.1:21533?cap_method=JAVACAP&&ori_method=MINICAPORI&&touch_method=MINITOUCH
+echo Loop
+goto loop
